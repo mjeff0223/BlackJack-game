8a7ee00c3c1d0e134a3b7c89407686922203a3bb
Markell <marketcreations2313@outlook.com>
2022-06-18 20:56:45 -0400

    Fixed issue where player could bust on initial deal

--- OOP_blackjack_final.py
+++ OOP_blackjack_final.py
@@ -10,13 +10,14 @@ print("*******      ******  *    *      *****     *       *     *         *    *
 
 # Coming Soon
 # add money
-# add additional players 
+# add additional players
 #num_players = input("1 player or 2 players? \n Type 1 or 2: ")
 name1 = input("\nEnter your name: ")
 # if num_players == 2:
-    # name2 = input("Enter name for player 2: ")
+# name2 = input("Enter name for player 2: ")
 deck = deck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 play = True
+
 
 class Dealer:
     def __init__(self):
@@ -26,7 +27,7 @@ class Dealer:
         self.hand = []
         total = sum(self.hand)
         return total
-    
+
     def deal_hand(self):
         while len(self.hand) <= 1:
             cards = random.choice(deck)
@@ -34,6 +35,7 @@ class Dealer:
         total = sum(self.hand)
         print(f"Dealer Hand: {self.hand[0]}")
         return total
+
     def draw_card(self):
         total = sum(self.hand)
         if sum(self.hand) < 17:
@@ -42,10 +44,11 @@ class Dealer:
             total = sum(self.hand)
             print(f"Dealer Hand: {total}")
             return total
-            
+
         else:
             print(f"Dealer Hand: {total}")
             return total
+
 
 class Player:
     def __init__(self):
@@ -55,12 +58,15 @@ class Player:
         self.hand = []
         total = sum(self.hand)
         return total
-    
+
     def deal_hand(self):
         while len(self.hand) <= 1:
             cards = random.choice(deck)
             self.hand.append(cards)
         total = sum(self.hand)
+        # This makes Ace equal to 1 automatically since the player would do so anyway in this situation
+        if total > 21:
+            total -= 10
         print(f"{name1}'s Hand: {total}")
 
     def draw_card(self):
@@ -74,6 +80,8 @@ class Player:
         total = sum(self.hand)
         print(f"{name1}: {total}")
         return total
+
+
 # Objects
 dealer = Dealer()
 player1 = Player()
@@ -84,32 +92,36 @@ dealer.deal_hand()
 player1.deal_hand()
 
 # Main Game
+
+
 def blackjack():
     play = True
     while play == True:
-        
+
         player_choice = input("\nChoose Hit or Stand: ")
         player_choice = player_choice.casefold()
         if player_choice == "hit":
             plyr_hand = player1.draw_card()
-            dealer_hand = dealer.draw_card() 
+            dealer_hand = dealer.draw_card()
             if plyr_hand > 21:
                 print("Bust! Dealer wins!")
                 play = False
             elif dealer_hand > 21:
                 print(f"Bust! {name1} wins!")
-                play = False 
+                play = False
             elif dealer_hand == plyr_hand:
                 print("Draw")
                 play = False
         elif player_choice == "stand":
             plyr_hand = player1.stand()
             dealer_hand = dealer.draw_card()
-            
+            if plyr_hand > 21:
+                print(f"Bust!, Dealer wins!")
+                break
             if dealer_hand > 21:
                 print(f"Bust! {name1} wins!")
                 break
-            # If neither the dealer nor the player has over 21 this part of the code determines which hand is closer to 21   
+            # If neither the dealer nor the player has over 21 this part of the code determines which hand is closer to 21
             plyr_hand_dis_21 = 21 - plyr_hand
             dealer_hand_dis_21 = 21 - dealer_hand
             if plyr_hand_dis_21 < dealer_hand_dis_21:
@@ -122,8 +134,8 @@ def blackjack():
                 print(f"Dealer hand is: {dealer_hand}")
                 print("Dealer wins!")
                 play = False
-        
-    
+
+
 blackjack()
 while True:
     play_again = input("\nPlay Again? Y or N: ")
@@ -131,7 +143,7 @@ while True:
     if play_again == "y":
         print("\n ---New Game---")
         player1.clear_hand()
-        dealer.clear_hand() 
+        dealer.clear_hand()
         player1.deal_hand()
         dealer.deal_hand()
         play = True
@@ -139,8 +151,4 @@ while True:
     elif play_again == "n":
         exit()
     else:
-        print("Invalid input")    
-
-    
-        
-        
+        print("Invalid input")
